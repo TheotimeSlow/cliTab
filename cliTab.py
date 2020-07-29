50321c67ae9f3cebf06592addb91e402d1411abf
TheotimeSlow <theoslow@yahoo.fr>
2020-07-29 17:59:10 +0200

    when typed play, the tab will be played with beeping sounds

--- cliTab.py
+++ cliTab.py
@@ -39,7 +39,7 @@ def menu(options):
 
 def EADG(tab):
 
-    #os.system('clear')
+    os.system('clear')
 
     f = Figlet(font='banner')
     print(f.renderText('EADG'))
@@ -59,19 +59,56 @@ def EADG(tab):
 
     eString = [41,44,46,49,52,55,58,62,65,69,73,78,82,87,92,98,104,110,117,123,131]
     eString2 = [329,349,369,391,415,440,466,493,523,554,587,622,659,698,739,783,830,880,932,989,1046]
-    
+    aString = [55,58,62,65,69,73,78,82,87,92,98,104,110,117,123,131,139,147,156,165,175]
+    aString2 = [440,466,469,523,554,587,622,659,698,740,784,831,880,932,988,1047,1109,1175,1245,1319,1397]
+    dString = [73,78,82,87,92,98,104,110,117,123,131,139,147,156,165,175,185,196,208,220,233]
+    dString2 = [587,622,659,698,740,784,831,880,932,988,1047,1109,1175,1245,1319,1397,1480,1568,1661,1760,1865]
+    gString = [98,104,110,117,123,131,139,147,156,165,175,185,196,208,220,233,247,262,277,298,311]
+    gString2 = [784,831,880,932,988,1047,1109,1175,1245,1319,1397,1480,1568,1661,1760,1865,1976,2093,2217,2349,2489]
     if n =='play':
-        print(tab[3])
-        for val in tab[3][1:]:
-            #print(tab[3].index(val))
-            #print(val)
+        position = 1
 
-            if val != '- ':
-                val = int(val)
+        print(tab[0])
+        print(tab[1])
+        print(tab[2])
+        print(tab[3])
+
+        while position < len(tab[0]):
+        #for valE in tab[3][1:]:
+
+            if tab[3][position] != '- ':
+                valE = int(tab[3][position])
                 #print('eString')
-                sine(eString2[val],0.25)
+                sine(eString2[valE],0.25)
+                
             else:
-                sleep(0.5)
+                pass
+        #for valA in tab[2][position:]:
+            if tab[2][position] != '- ':
+                print('aString')
+                valA = int(tab[2][position])
+                sine(aString2[valA],0.25)
+            else:
+                pass
+        #for valD in tab[1][position:]:
+            if tab[1][position] != '- ':
+                print('dString')
+                valD = int(tab[1][position])
+                sine(dString2[valD],0.25)
+            else:
+                pass
+        #for valG in tab[0][position:]:
+            if tab[0][position] != '- ':
+                print(tab[0][position:])
+                print('gString')
+                valG = int(tab[0][position])
+                sine(gString2[valG],0.25)
+            else:
+                sleep(0.25)
+            
+            position = position+1
+            print(position)
+                
 
 
     if n == 'pass':
@@ -85,7 +122,7 @@ def EADG(tab):
 
     if len(n) == 3 :
         note = n[1] + n[2]
-    else:
+    elif len(n) == 2:
         note = n[1] + ' '
 
 
